eb53153f5bcd5be23aad3c1f8e6415c26361d139
Roman Skok <rma.skok@gmail.com>
2023-12-07 00:29:45 +0200

    feat: card design and logic

--- src/WorkerComponent/TeamMember.js
+++ src/WorkerComponent/TeamMember.js
@@ -1,13 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './TeamMember.css';
 
 const TeamMember = ({ name, surname, position, photoUrl, color }) => {
+    const [isFullscreen, setIsFullscreen] = useState(false);
     const dynamicStyle = {
         color: color || 'black',
     };
 
+    const handleCardClick = () => {
+        setIsFullscreen(!isFullscreen);
+    };
+    const cardClassName = `worker-info border p-3 bg-light m-2${isFullscreen ? ' fullscreen' : ''}`;
+
     return (
-        <div className="worker-info border p-3 bg-light m-2">
+        <div className={cardClassName} onClick={handleCardClick}>
             <div className="name-surname">
                 <p className={"Name"}>{name} {surname}</p>
             </div>
